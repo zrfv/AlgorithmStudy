63f0c48f6a7d91ab91d7c1e040d9004ad160a08a
Zrfv <youngoh07015@gmail.com>
2024-10-11 10:45:44 +0900

    add test code

--- src/test/java/algorithms/FibonacciTest.java
+++ src/test/java/algorithms/FibonacciTest.java
@@ -1,4 +1,22 @@
 package algorithms;
 
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class FibonacciTest {
+    @Test
+    public void testFib() {
+        Fibonacci fibonacci = new Fibonacci();
+
+        Assert.assertEquals(0, fibonacci.fib(0));
+        Assert.assertEquals(1, fibonacci.fib(1));
+        Assert.assertEquals(1, fibonacci.fib(2));
+        Assert.assertEquals(2, fibonacci.fib(3));
+        Assert.assertEquals(3, fibonacci.fib(4));
+        Assert.assertEquals(5, fibonacci.fib(5));
+        Assert.assertEquals(8, fibonacci.fib(6));
+        Assert.assertEquals(13, fibonacci.fib(7));
+
+    }
 }
